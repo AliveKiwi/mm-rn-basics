a098a02613c0188100acc6bcdad88661e0bda46c
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-19 02:20:20 +0530

    16 basics: Replaced default content

--- App.js
+++ App.js
@@ -1,29 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text>Another piece of text!</Text>
+        <TextInput placeholder="Your course goal!"></TextInput>
+        <Button title="Add Goal" />
       </View>
-      <Text
-        style={styles.dummyText}
-        // style={{ margin: 16, borderWidth: 2, borderColor: 'red', padding: 16 }}
-      >
-        Hello World!
-      </Text>
-      <Button title="Tap Me!" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+    borderColor: 'red',
+    borderWidth: 2,
   },
-  dummyText: { margin: 16, borderWidth: 2, borderColor: 'red', padding: 16 },
 });
