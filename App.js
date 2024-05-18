2dbcc78976adae39f109f4621f1ade6bcca8b532
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-19 02:27:55 +0530

    19 basics: Added flex and basics css like padding, margin, width, border to textInput

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!"></TextInput>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+        ></TextInput>
         <Button title="Add Goal" />
       </View>
       <View>
@@ -20,5 +23,16 @@ const styles = StyleSheet.create({
     padding: 50,
     borderColor: 'red',
     borderWidth: 2,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
   },
 });
